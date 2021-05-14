4ab3a0d2ec996896148cd133453dd35b3c1a5331
Pedro Barros <pedrobars7a@gmail.com>
2021-05-14 14:15:23 -0300

    Ajustando links do backend

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2021
+            <a href='http://linkedin.com/in/pedrobadm' target='_blank'> Pedro Barros</a>.
+        </strong>
+    </footer>
+)
---
+++ src/const.js
@@ -0,0 +1,4 @@
+export default { 
+    API_URL: 'https://my-money-project-backend.herokuapp.com/api',
+    OAPI_URL: 'https://my-money-project-backend.herokuapp.com//oapi',
+}
